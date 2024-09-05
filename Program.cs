be25f6f5191c7a52adbb1d930599b0e9b26dce8d
amhc082024 <amhc082024@gmail.com>
2024-09-05 11:30:19 +0200

    v2 funker ikke

--- Program.cs
+++ Program.cs
@@ -1,17 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+class Movie
+{
+    public string Title {get; set;}
 
-// Get all movies
-app.MapGet("/movies", () => "Return all movies");
-//UPDATE:Update a movie
-app.MapPut("/movies/{Id}", (int Id) => $"Update movie with id: {Id}");
-//CREATE:Adds a mpvoe
-app.MapPost("/movies", () => "Add a new movie");
-//DELETE: Delete a movie with id
-app.MapDelete("/movies/{Id}", (int Id) => $"Delete movie with id: {Id}" );
+    public Movie(string title)
+    {
+        Title = title;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddSingleton<List<Movie>>(); 
+        //Register the list with dependency injection container
+        var app = builder.Build();
+
+        // Get all movies
+        app.MapGet("/movies", (List<Movie> movies) => movies);
+        //UPDATE:Update a movie
+        app.MapPut("/movies/{Id}", (int Id) => $"Update movie with id: {Id}");
+        //CREATE:Adds a mpvoe
+        app.MapPost("/movies", (Movie? movie, List<Movie> movies) () => 
+        {   
+           if [Movie == null]
+           {
+            return Results.BadRequest();
+           } 
+           movies.Add(movie);
+
+           return Results.Created(); 
+        });
+        //DELETE: Delete a movie with id
+        app.MapDelete("/movies/{Id}", (int Id) => $"Delete movie with id: {Id}");
 
 
-app.MapGet("/health", () => "hola bitch!!");
+        app.MapGet("/health", () => "hola bitch!!");
 
 
-app.Run();
+        app.Run();
+    }
+}
